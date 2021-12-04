ca04bf2ce9f826842e3822f6089809b08688c231
powerfulyang <i@powerfulyang.com>
2021-12-04 23:53:45 +0800

    feat: useImmer

--- src/__test__/immer.spec.ts
+++ src/__test__/immer.spec.ts
@@ -1,26 +1,40 @@
-import { produce } from 'immer';
+import { produce, setAutoFreeze } from 'immer';
+import { isVoid } from '@powerfulyang/utils';
 
 describe('test immer', () => {
+  beforeAll(() => {
+    setAutoFreeze(false);
+  });
+
   it('produce setter function', () => {
     const val = { a: 1 };
     const produceSetter = produce((draft) => {
       draft.a = 2;
     });
-    expect(produceSetter(val)).toStrictEqual({ a: 2 });
     expect(val).toStrictEqual({ a: 1 });
+    expect(produceSetter(val)).toStrictEqual({ a: 2 });
   });
 
   it('produce next state', () => {
     const baseState = { a: 1 };
     const newState = produce(baseState, (draft) => {
-      draft.a = 2;
+      draft.a += 1;
     });
-    expect(baseState).toStrictEqual(baseState);
+    expect(baseState).toStrictEqual({ a: 1 });
     expect(newState).toStrictEqual({ a: 2 });
+  });
+
+  it('produce next state with setter', () => {
+    const baseState = { a: 1 };
+    const newState = produce(baseState, () => {
+      return 2;
+    });
+    expect(baseState).toStrictEqual({ a: 1 });
+    expect(newState).toStrictEqual(2);
   });
 
   it('test void function return value', () => {
     const func = () => {};
-    expect(func.call(this)).toBeUndefined();
+    expect(isVoid(func.call(this))).toBe(true);
   });
 });
--- src/hooks/useImmer.ts
+++ src/hooks/useImmer.ts
@@ -1,17 +1,19 @@
-import { enableAllPlugins, produce } from 'immer';
-import { useCallback, useState } from 'react';
+import { enableAllPlugins, produce, setAutoFreeze } from 'immer';
+import { useCallback, useRef, useState } from 'react';
 import type { ReturnTypedFunction, VoidFunction } from '@powerfulyang/utils';
-import { isVoid, isFunction } from '@powerfulyang/utils';
+import { isFunction } from '@powerfulyang/utils';
 
 enableAllPlugins();
 
+setAutoFreeze(false);
+
 export function useImmer<T>(
   initialState: T | ReturnTypedFunction<T>,
-): [T, VoidFunction<[T | VoidFunction<[T]> | ReturnTypedFunction<T>]>, VoidFunction];
+): [T, VoidFunction<[T | VoidFunction<[T]> | ReturnTypedFunction<T, [T]>]>, VoidFunction];
 
-export function useImmer<T = undefined>(): [
+export function useImmer<T>(): [
   T | undefined,
-  VoidFunction<[T | VoidFunction<[T]> | ReturnTypedFunction<T>]>,
+  VoidFunction<[T | VoidFunction<[T | undefined]> | ReturnTypedFunction<T, [T | undefined]>]>,
   VoidFunction,
 ];
 
@@ -20,24 +22,20 @@ export function useImmer<T = undefined>(): [
  * @param initialValue
  * @returns [state, setState, resetState]
  */
-export function useImmer<T = any>(initialValue?: T | ReturnTypedFunction<T>) {
+export function useImmer<T>(initialValue?: T | ReturnTypedFunction<T, []>) {
+  const initialValueRef = useRef(initialValue);
   const [value, updateValue] = useState(initialValue);
-  const setState = useCallback(
-    (updater: T | VoidFunction<[T | undefined]> | ReturnTypedFunction<T>) => {
-      if (isFunction(updater)) {
-        updateValue((v) => {
-          const returnValue = updater(v);
-          if (isVoid(returnValue)) {
-            return produce(v, updater);
-          }
-          return returnValue;
-        });
-      }
-      return updateValue(updater);
-    },
-    [],
-  );
-  const resetState = useCallback(() => updateValue(initialValue), [initialValue]);
+  const setState = useCallback((updater: T | VoidFunction<[T]> | ReturnTypedFunction<T, [T]>) => {
+    if (isFunction(updater)) {
+      return updateValue((v) => {
+        return produce(v, updater);
+      });
+    }
+    return updateValue(updater);
+  }, []);
+  const resetState = useCallback(() => {
+    return updateValue(initialValueRef.current);
+  }, []);
 
   return [value, setState, resetState] as const;
 }
