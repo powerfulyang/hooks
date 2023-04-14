c51166511461ba53cb8c3c4d172ca9887913455c
powerfulyang <i@powerfulyang.com>
2023-04-14 11:35:34 +0800

    chore: monorepo type declaration

--- jest.config.js
+++ jest.config.js
@@ -5,9 +5,7 @@ const moduleNameMapper = pathsToModuleNameMapper(tsconfig.compilerOptions.paths,
   prefix: '<rootDir>/',
 });
 
-/**
- * @type {import('@jest/types').Config.InitialOptions}
- */
+/** @type {import('jest').Config} */
 module.exports = {
   moduleNameMapper,
   transform: {
